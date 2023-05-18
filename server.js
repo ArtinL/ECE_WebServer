a5c204954aee2dc92d4fce6eeb7d67b479cf4911
ArtinL <artinlahni@gmail.com>
2023-05-18 23:12:27 +0000

    Changed to ancient JavaScript to match with board

--- server.js
+++ server.js
@@ -1,40 +1,44 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+var express = require('express');
+var app = express();
+var port = 3000;
 
-let throttle;
-let status; 
+var throttle;
+var status; 
 
 app.use(express.static('public'));
 
-app.get('/motor/on', (req, res) => {
+app.get('/motor/on', function(req, res) {
 
   // Code to turn the motor on
-  
+  console.log("Server received motor on");
   status = 'Idle';
   res.send(status);
 });
 
-app.get('/motor/off', (req, res) => {
+app.get('/motor/off', function(req, res) {
 
   // Code to turn the motor off
+
+  console.log("Server received motor off");
   
   status = 'Off';
   res.send(status);
 });
 
-app.get('/motor/control', (req, res) => {
+app.get('/motor/control', function (req, res) {
 
     throttle = req.query.number;
 
     // Code to control the motor based on the number
+
+    console.log("Server received throttle: " + throttle);
     
-    status = throttle == 0 ? "Idle" : `Running at ${(throttle*100).toFixed(0)}% throttle`;
+    status = throttle == 0 ? "Idle" : "Running at " + (throttle*100).toFixed(0)  + " % throttle";
     res.send(status);
 });
 
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(port, function() {
+  console.log("Server running on port" + port);
 });
