5542da7aad660464ca7a964019a842c026338d78
ArtinL <artinlahni@gmail.com>
2023-05-21 13:07:21 -0700

    Bug fixes

--- mem_write.c
+++ mem_write.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/mman.h>
-#include <string.h>
 
 #define GPIO1_BASE_ADDRESS 0xFF708000
 #define GPIO1_REGISTER_SIZE 0x1000
